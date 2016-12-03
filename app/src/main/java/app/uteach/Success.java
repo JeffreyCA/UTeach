7ba7cd7c2b9a1a3dc2c35480b0d8a25c9ca0b849
Jeffrey <jeffreyca16@gmail.com>
2016-12-03 17:02:18 -0500

    Success Page

---
+++ app/src/main/java/app/uteach/Success.java
@@ -0,0 +1,13 @@
+package app.uteach;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Success extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutor_complete);
+    }
+}
